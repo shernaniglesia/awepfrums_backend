79b846b6e2ae9769bfb7eb5b6301989ad2146598
shernaniglesia <shernaniglesia@gmail.com>
2026-08-24 14:18:01 +0800

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+require('dotenv').config();
 
 const pool = mysql.createPool({
   host:     process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 25691,
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
